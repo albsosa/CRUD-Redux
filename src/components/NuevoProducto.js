908ed698c78d069dec1a700572db66b06cdbee10
albsosa <inf.albsosa@gmail.com>
2019-09-09 22:28:48 -0500

    Validando formulario

--- src/components/NuevoProducto.js
+++ src/components/NuevoProducto.js
@@ -1,40 +1,51 @@
 import React, { useState } from 'react';
 
-//Redux
-import {crearNuevoProductoAction} from '../actions/productosActions';
-import {useDispatch} from 'react-redux';
-const NuevoProducto = () => {
+// Redux
+import { crearNuevoProductoAction } from '../actions/productosActions';
+import { validarFormularioAction, validacionExito, validacionError } from '../actions/validacionActions';
+import { useDispatch, useSelector } from 'react-redux';
 
-   // sate
-   const [nombre, guardarNombre] = useState('');
-   const [precio, guardarPrecio] = useState('');
+const NuevoProducto = ({history}) => {
 
-   //crear nuevo producto
+    // sate
+    const [nombre, guardarNombre] = useState('');
+    const [precio, guardarPrecio] = useState('');
 
-   const dispatch = useDispatch();
-   const agregarProducto = (producto) => dispatch(crearNuevoProductoAction(producto));
 
-    //cuando el formulario es enviado
+    // Crear nuevo producto 
+    const dispatch = useDispatch();
+    const agregarProducto = (producto) => dispatch(  crearNuevoProductoAction(producto) );
+    const validarFormulario = () => dispatch( validarFormularioAction() );
+    const exitoValidacion = () => dispatch( validacionExito() ) ;
+    const errorValidacion = () => dispatch( validacionError() ) ;
+
+    // Obtener los datos del state
+    const error = useSelector((state) => state.error.error);
+
+    // Agregar nuevo producto
     const submitNuevoProducto = e => {
-    e.preventDefault(); 
+        e.preventDefault();
 
-    agregarProducto({
-        nombre,
-        precio
-    });
+        validarFormulario();
+
+        // Validar el formulario
+        if(nombre.trim() === '' || precio.trim() === '') {
+            errorValidacion();
+            return;
+        }
+
+        // Si pasa la validación
+        exitoValidacion();
+
+        // crear el nuevo producto
+        agregarProducto({
+            nombre, precio
+        });
+
+        // redireccionar
+        history.push('/');
 
     }
-
-     // Validar el formulario
-     if(nombre.trim() === '' || precio.trim() === '') {
-        console.log('error validacion')
-      
-    }
-
-    //crear el producto
-
-    //redireccionar 
-
 
 
     return (
@@ -67,6 +78,8 @@ const NuevoProducto = () => {
 
                             <button type="submit" className="btn btn-primary font-weight-bold text-uppercase d-block w-100">Agregar</button>
                         </form>
+
+                        { error ? <div className="font-weight-bold alert alert-danger text-center mt-4">Todos los campos son obligatorios</div> : null }
         
                     </div>
                 </div>
